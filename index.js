5fc5821ff3eb480ed1544e175346b0fb9c453032
Nick The Sick <nperez0111@gmail.com>
2016-08-29 19:11:26 -0700

    -mpretty much wrote all of the extender

--- index.js
+++ index.js
@@ -1,10 +1,80 @@
-'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+const isFunc = require( 'is-function' ),
+    isObj = require( 'is-object' ),
+    mapObj = require( 'map-obj' ),
+    isCorrectType = obj => {
+        return Object.keys( obj ).map( key => {
 
-	opts = opts || {};
+            return obj[ key ]
 
-	return input + ' & ' + (opts.postfix || 'rainbows');
-};
+        } ).map( val => {
+
+            if ( isObj( val ) ) {
+
+                return isCorrectType( val )
+
+            }
+
+            return isFunc( val )
+
+        } ).every( isTrue => {
+
+            return isTrue
+
+        } );
+
+    },
+    mapToSymbols = ( obj ) => {
+
+        return mapObj( obj, ( key, val ) => {
+
+            if ( isObj( val ) ) {
+
+                return [ key, mapToSymbols( val ) ];
+
+            }
+
+            return [ key, Symbol( key ) ];
+
+        } )
+
+    },
+    funcBinder = ( obj, symbols, toBeBound ) => {
+
+        return mapObj( obj, ( key, val ) => {
+            if ( isObj( val ) ) {
+
+                return [ key, funcBinder( val, symbols[ key ], toBeBound ) ]
+
+            }
+
+            return [ key, function () {
+
+                let args = Array.from( arguments ).unshift( this )
+
+                return val.apply( this, args )
+
+            } ]
+
+        } )
+
+    }
+
+module.exports = ( objToExtendWith ) => {
+
+    if ( !isCorrectType( objToExtendWith ) ) {
+
+        return false;
+
+    }
+
+    let symbols = mapToSymbols( objToExtendWith ),
+        vals = funcBinder( objToExtendWith, symbols )
+
+    Object.keys( vals ).forEach( function ( key ) {
+
+        Object.prototype[ symbols[ key ] ] = vals[ key ]
+
+    } )
+
+    return symbols;
+}
